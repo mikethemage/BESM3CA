26ca1f830d5257e9cde19fad62481e3e875a0329
Mike Dunn <44652427+mikethemage@users.noreply.github.com>
2024-08-09 12:51:19 +0100

    Removed Triarch data to move to Triarch project/solution

--- Triarch.BusinessLogic/Services/IRPGSystemProvider.cs
+++
@@ -1,8 +0,0 @@
-﻿using Triarch.BusinessLogic.Models.Definitions;
-
-namespace Triarch.BusinessLogic.Services;
-public interface IRPGSystemProvider
-{
-    void AddSystem(string name, RPGSystem system);
-    RPGSystem LoadSystem(string name);
-}
--- Triarch.Prototype/ViewModels/AllowedChildrenViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Triarch.Prototype.ViewModels;
-
-public class AllowedChildrenViewModel
-{
-    public List<FilterTypeViewModel> FilterList { get; set; } = new List<FilterTypeViewModel>();
-    public FilterTypeViewModel SelectedFilter { get; set; } = null!;
-    public List<ElementDefinitionListItemViewModel> AllowedChildrenList { get; set; } = new List<ElementDefinitionListItemViewModel>();
-    public ElementDefinitionListItemViewModel SelectedChild { get; set; } = null!;
-}
